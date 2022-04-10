0923c224e6fa188dc40309b28f6acd00bc274481
fgclue <lost@biitle.nl>
2022-04-10 13:00:12 -0300

    IC BETA 1.0.0

--- IC/app.js
+++ IC/app.js
@@ -4,6 +4,7 @@ import chalk from 'chalk';
 import figlet from 'figlet';
 import { createSpinner } from 'nanospinner';
 import inquirer from 'inquirer';
+import gradient from 'gradient-string';
 import chalkAnimation from 'chalk-animation';
 
 let PlayerName;
@@ -14,7 +15,6 @@ async function welcome() {
     const rainbowTitle = chalkAnimation.rainbow(
         'Imunização Coletiva \n'
     );
-
     await sleep();
     rainbowTitle.stop();
 
@@ -22,12 +22,13 @@ async function welcome() {
     ${chalk.bgBlueBright('Como Jogar')}
     Ola!
 
-    ${chalk.red('O')} Virus ${chalk.blue('I')} Pessoa ${chalk.bgWhite(' ')} Centro ${chalk.bgGreenBright(' ')} Grama
+    ${chalk.red('O')} Virus ${chalk.blue('I')} Pessoa ${chalk.bgWhite(' ')} Centro ${chalk.bgGreenBright(' ')} Grama ${chalk.blueBright('Ǝ')} Pessoa Com Mascara ${chalk.whiteBright('/')} Vacina
     `)
 }
 
 await welcome()
 await askName()
+await winner()
 
 async function askName() {
     const anwsers = await inquirer.prompt({
@@ -42,7 +43,18 @@ async function askName() {
     PlayerName = anwsers.player_name;
 }
 
-const spinner = createSpinner('Carregando Jogo...').start();
+const spinner = createSpinner('Carregando Jogo...\n').start();
 await sleep();
 
 spinner.success({ text: `Jogo Carregado!` });
+
+function winner() {
+    console.clear();
+    figlet(`Você Completou O Jogo!`, (err, data) => {
+      console.log(gradient.pastel.multiline(data) + '\n');
+    // Yea
+    const msg2 = `Obrigado por jogando, ${PlayerName}!\n`
+    console.log(msg2)
+    process.exit(0);
+    });
+  }
